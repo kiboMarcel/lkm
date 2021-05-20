b978c31a97ddcdc6a2e0230f4207863713bcff05
marcel <nouletamemarcel@gmail.com>
2021-05-20 12:43:56 +0000

    first commit

---
+++ base/views/product_views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from base.models import Product
+from base.serializers import ProductSerializer
+from rest_framework import status
+
+
+
+@api_view(['GET'])
+def getProducts(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getProduct(request, pk):
+  #  product = None
+   # for i in Product:
+    #    if i['_id'] == pk:
+     #       product = i
+      #      break
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)   
